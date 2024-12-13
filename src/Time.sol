bf6ce92a633afba0527d08e9afdf11da41160e55
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-12-13 07:24:24 -0500

    10 time start Time.sol

---
+++ src/Time.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract Auction {
+    uint256 public startAt = block.timestamp + 1 days;
+    uint256 public endAt = block.timestamp + 2 days;
+
+    function bid() external{
+        require(block.timestamp < startAt, "auction has not started yet");
+        require(block.timestamp > endAt, "auction has ended");
+    }
+
+    function end() external {
+        require(block.timestamp > endAt, "auction has not ended yet");
+    }
+}
