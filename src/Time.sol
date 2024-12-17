9ccde111c92c9b1f47195ea9a7d282734e73a400
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-12-17 07:40:11 -0500

    correct error in Time.sol and test pass

--- src/Time.sol
+++ src/Time.sol
@@ -5,12 +5,12 @@ contract Auction {
     uint256 public startAt = block.timestamp + 1 days;
     uint256 public endAt = block.timestamp + 2 days;
 
-    function bid() external{
-        require(block.timestamp < startAt, "auction has not started yet");
-        require(block.timestamp > endAt, "auction has ended");
+    function bid() external view {
+        require(block.timestamp > startAt, "auction has not started yet");
+        require(block.timestamp < endAt, "auction has ended");
     }
 
-    function end() external {
+    function end() external view {
         require(block.timestamp > endAt, "auction has not ended yet");
     }
 }
