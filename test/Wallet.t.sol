6073a78592a31f1de7881db0b6de7b80d63c19d3
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-12-17 10:39:15 -0500

    11 3:50 deal test pass

--- test/Wallet.t.sol
+++ test/Wallet.t.sol
@@ -4,13 +4,12 @@ pragma solidity 0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {Wallet} from "src/Wallet.sol";
 
-
 // Examples of deal and hoax
 // deal(address, uint) - Set balance of address
 // hoax(address, uint) - deal + prank, Sets up a prank and set balance
 
 contract WalletTest is Test {
-    Wallet public wallet; 
+    Wallet public wallet;
 
     function setUp() public {
         wallet = new Wallet{value: 1e18}();
@@ -20,5 +19,19 @@ contract WalletTest is Test {
         (bool ok,) = address(wallet).call{value: _amount}("");
         require(ok, "send failed");
     }
-    
+
+    function testEthBalance() public view {
+        console.log("ETH balance: ", address(this).balance / 1e18);
+    }
+
+    function testSendEth() public {
+        // deal(address, uint) - Set balance of address
+        deal(address(1), 100);
+        assertEq(address(1).balance, 100);
+
+        deal(address(1), 10);
+        assertEq(address(1).balance, 10);
+
+        // hoax(address, uint) - deal + prank, Sets up a prank and set balance
+    }
 }
